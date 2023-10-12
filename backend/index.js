7f113bf43698cb36ac0112e28b5cc336987ae322
Dhanush <dhanushg295@gmail.com>
2023-10-11 20:45:31 -0700

    added users

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const adimnroutes = require("./routes/admin");
+const userroutes = require("./routes/users");
 const { default: mongoose } = require("mongoose");
 
 app.use(express.json());
 app.use('/admin', adimnroutes);
+app.use('/users', userroutes);
 
 
 mongoose
