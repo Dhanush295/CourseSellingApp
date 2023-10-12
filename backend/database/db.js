d931bc20c2e8cf52bfa6310b7df3b809902a501c
Dhanush <dhanushg295@gmail.com>
2023-10-11 20:43:50 -0700

    updated DB

--- backend/database/db.js
+++ backend/database/db.js
@@ -6,5 +6,11 @@ const AdminSchema = new mongoose.Schema({
     password: String
 })
 
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+})
+
 const ADMIN = mongoose.model('Admin', AdminSchema);
+const USER = mongoose.model('User', UserSchema);
 module.exports = { ADMIN };
